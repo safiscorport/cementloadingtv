b5a0f3f65a16cf8a63c91f76eb47f80587f177e7
safiscorport <safiscorport@gmail.com>
2026-09-07 22:50:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const $ = id => document.getElementById(id);
 const num = v =>
   (v === undefined || v === null || v === 0 || v === '-')
     ? (v === 0 ? '0' : (v || '—'))
-    : Number(v).toLocaleString('en-US');
+    : Number(v).toLocaleString('en-US');SC
 
 
 function formatActivityTime(val) {
